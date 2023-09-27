ac991742241eb096e5c4957ea662001feb1980eb
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-09-26 17:50:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Github_Practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first assignment wooooo");
+        }
+    }
+}
